ceb96db9abaf404974a5ff2f14851467f829b98e
Lisette1701 <lisettegomez@liceovvh.cl>
2025-09-29 11:14:35 -0300

    ok

---
+++ Lisette-main/3,3/script.js
@@ -0,0 +1,21 @@
+function liked(element) {
+    let likeSpan = element.querySelector('span');
+    let incrementarLikes = parseInt(likeSpan.innerText);
+    incrementarLikes = incrementarLikes + 1;
+    likeSpan.innerText = incrementarLikes;
+}
+
+
+
+function login(element) {
+    if (element.innerText === "Login") {
+        element.innerText = "Logout";
+    } else {
+        element.innerText = "Login";
+    }
+}
+
+function hide(element) {
+    // Oculta el botón.
+    element.style.display = 'none';
+}
